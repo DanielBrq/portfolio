8c6e4c6c5733da17a421457d81cee5912014d285
Daniel <daniel.barquero.dev@gmail.com>
2026-05-27 09:37:21 -0600

    First Commit

---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+
+<template>
+  <h1 class="text-red-500 bg-black h-screen w-full">Home</h1>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+import { defineNuxtConfig } from "nuxt/config";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  ssr: false,
+  devtools: { enabled: true },
+
+  vite: {
+    plugins: [
+      tailwindcss()
+    ],
+  },
+
+  css: ['@/assets/css/main.css']
+})
