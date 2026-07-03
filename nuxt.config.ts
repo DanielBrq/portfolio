b8dff633eab6edce3a880845d50d1a0c249bbb7f
Daniel <daniel.barquero.dev@gmail.com>
2026-07-03 14:00:38 -0600

    fix: allow vite 8

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   experimental: {
     componentIslands: true,
+    viteEnvironmentApi: true,
   },
   nitro: {
     routeRules: {
